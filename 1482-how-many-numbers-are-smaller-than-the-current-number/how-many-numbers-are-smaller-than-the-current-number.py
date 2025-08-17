516099f86c907f730fd7d31de250cff2764be381
yerosentamrat63 <162616048+yerosentamrat63@users.noreply.github.com>
2025-08-17 15:16:41 +0300

    how-many-numbers-are-smaller-than-the-current-number.py

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -8,3 +8,15 @@ class Solution:
                     counter[i] += 1
         return(counter)
 
+#Using Hashmap O(n logn)
+
+"""
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        sorted_nums = sorted(nums)  
+        mapping = {}
+        for i, num in enumerate(sorted_nums):
+            if num not in mapping:   # store only first occurrence
+                mapping[num] = i
+        return [mapping[num] for num in nums]
+"""
