cde45a2b9f80239682a3ffb4c93a474402d8102e
yerosentamrat63 <162616048+yerosentamrat63@users.noreply.github.com>
2025-08-18 11:50:01 +0300

    remove-duplicates-from-sorted-array.py

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,9 +1,26 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        i = 1
+        while i < len(nums):
+            if nums[i] == nums[i - 1]:
+                nums.pop(i)
+            else:
+                i += 1 
+        return len(nums)
+#===============> O(n²)
+
+
+"""
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
         i = 0 
         for j in range(1, len(nums)):
             if nums[j] != nums[i]:
                 i += 1
                 nums[i] = nums[j]
-        
         return i + 1
+"""
+#=================>O(n)
