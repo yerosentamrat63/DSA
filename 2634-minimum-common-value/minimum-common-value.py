b1e05e4b6314c5233eabcb093963208a02534fcc
yerosentamrat63 <162616048+yerosentamrat63@users.noreply.github.com>
2025-08-20 16:37:31 +0300

    minimum-common-value.py

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -16,3 +16,18 @@ class Solution:
         if current_max == 0:
             return(-1)
         return (current_max)
+
+#better approach
+"""
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        i, j = 0, 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:  # nums1[i] == nums2[j]
+                return nums1[i]
+        return -1
+"""
