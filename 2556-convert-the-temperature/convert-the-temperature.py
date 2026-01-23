31f936ab97b8e7f06f5dc9a8836b4265aae8df89
Yerosen Tamrat <162616048+yerosentamrat63@users.noreply.github.com>
2026-01-23 21:03:15 +0300

    Time: 0 ms (100.00%) | Memory: 19.6 MB (13.32%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.py
+++ 2556-convert-the-temperature/convert-the-temperature.py
@@ -2,10 +2,5 @@ class Solution:
     def convertTemperature(self, celsius: float) -> List[float]:
         k = celsius + 273.15
         f = celsius * 1.80 + 32.00
-        ans = [k,f]
+        ans = [k, f]
         return ans
-
-"""
-Time complexity: O(1)
-Space complexity: O(1)
-"""
