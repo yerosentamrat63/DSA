51b2c025e33a340bfe8dfaf3dba01c9117d2be8b
Yerosen Tamrat <162616048+yerosentamrat63@users.noreply.github.com>
2026-02-07 22:41:39 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (13.15%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,22 +1,12 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        leng = len(nums)
-        counter = [0] * leng
-        for i in range(leng):
-            for j in range(leng):
-                if nums[i] > nums[j]:
-                    counter[i] += 1
-        return(counter)
+        sorted_nums = sorted(nums)
+        rank = {}
+        res = []
 
-#Using Hashmap O(n logn)
-
-"""
-class Solution:
-    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        sorted_nums = sorted(nums)  
-        mapping = {}
         for i, num in enumerate(sorted_nums):
-            if num not in mapping:   # store only first occurrence
-                mapping[num] = i
-        return [mapping[num] for num in nums]
-"""
+            if num not in rank:
+                rank[num] = i
+        for num in nums:
+            res.append(rank[num])
+        return res
