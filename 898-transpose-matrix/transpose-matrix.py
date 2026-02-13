ef11d5e707046dfd8594a2e07415f0ff32425f9a
Yerosen Tamrat <162616048+yerosentamrat63@users.noreply.github.com>
2026-02-13 20:45:04 +0300

    Time: 0 ms (100.00%) | Memory: 20 MB (10.75%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.py
+++ 898-transpose-matrix/transpose-matrix.py
@@ -1,4 +1,4 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        zipped = list(zip(*matrix))
-        return (list(map(list, zipped)))
+        transpose = list(map(list, zip(*matrix)))
+        return transpose
