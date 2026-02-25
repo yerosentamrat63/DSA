a7e7baf0344de49cfbe4818bb21709eac6afb1f2
Yerosen Tamrat <162616048+yerosentamrat63@users.noreply.github.com>
2026-02-25 23:34:22 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (54.86%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,15 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        ones = 0
-        zeros = 0
-        twos = 0
-        for i in nums:
-            if i == 1:
-                ones += 1
-            elif i == 0:
-                zeros += 1
-            elif i == 2:
-                twos += 1
-        res = zeros * [0] + ones * [1] + twos * [2]
-        nums[:] = res[:]
+        zero = 0
+        one = 0
+        two = 0
+        for num in nums:
+            if num == 0:
+                zero += 1
+            elif num == 1:
+                one += 1
+            else:
+                two += 1
+        final = [0] * zero + [1] * one + [2] * two
+        nums[:] = final[:]
