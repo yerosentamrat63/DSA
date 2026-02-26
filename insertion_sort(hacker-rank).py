baf0c3ab478e3891bbf190bb75b0f31d2170b27d
Yerosen Tamrat <162616048+yerosentamrat63@users.noreply.github.com>
2026-02-26 20:49:16 +0300

    Add insertionSort1 function for sorting an array
    Implement insertion sort algorithm and print steps.

---
+++ insertion_sort(hacker-rank).py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'insertionSort1' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+
+def insertionSort1(n, arr):
+    # Write your code here
+    num = arr[-1]
+    i = n - 2
+    while num < arr[i] and i >= 0:
+        arr[i + 1] = arr[i]
+        print(*arr)
+        i -= 1
+    arr[i + 1] = num
+    print(" ".join(map(str, arr)))
+        
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
