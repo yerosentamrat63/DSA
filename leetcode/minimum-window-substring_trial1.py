46b7bff9dee67d861a1d2382dd5b6cd1e1e43f49
Yerosen Tamrat <162616048+yerosentamrat63@users.noreply.github.com>
2026-03-15 19:05:31 +0300

    Add solution for Minimum Window Substring (trial 1)

---
+++ leetcode/minimum-window-substring_trial1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:  
+        need = Counter(t)
+        missing = len(t)
+        left = start = end = 0     
+
+        for right, char in enumerate(s, 1):
+            if need[char] > 0:
+                missing -= 1
+            need[char] -= 1
+            if missing == 0:
+                while left < right and need[s[left]] < 0:
+                    need[s[left]] += 1
+                    left += 1
+
+                if end == 0 or right - left < end - start:
+                    start, end = left, right
+
+                need[s[left]] += 1
+                missing += 1
+                left += 1
+        return s[start:end]
