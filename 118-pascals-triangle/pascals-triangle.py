9b977768501c28232a0ecb64b2d8bd79e522ccc9
Yerosen Tamrat <162616048+yerosentamrat63@users.noreply.github.com>
2026-04-15 21:43:29 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (74.78%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,21 +1,10 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        """
-        if numRows == 0:
-            return []
-
-        initialize res = [[1]]
-        shift right [0121]
-        shift left  [1210]
-        sum         ]1331]
-        """
-
         res = [[1]]
-
-        for i in range(numRows - 1):
+        for idx in range(numRows - 1):
             prev = [0] + res[-1] + [0]
-            row = []
-            for j in range(len(res[-1]) + 1):
-                row.append(prev[j] + prev[j+1])
-            res.append(row)
+            subarr = []
+            for jdx in range(len(res[-1])+ 1):
+                subarr.append(prev[jdx] + prev[jdx+1])
+            res.append(subarr)
         return res
